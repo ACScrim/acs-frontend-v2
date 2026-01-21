97fe51f17fadccd3f3c9f87034a9d1e4c52fd048
Aubrian <aubriduhay@gmail.com>
2026-01-21 22:54:39 +0100

    Meilleur gestion des erreurs

--- src/components/global/ToastContainer.vue
+++ src/components/global/ToastContainer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Teleport } from 'vue';
 import Toast from '../ui/Toast.vue';
 import { useToastStore } from '../../stores/toastStore';
 
@@ -6,17 +7,19 @@ const toastStore = useToastStore();
 </script>
 
 <template>
-  <div class="fixed top-4 right-4 z-50 flex flex-col gap-2 pointer-events-none">
-    <Toast
-      v-for="toast in toastStore.toasts"
-      :key="toast.id"
-      :id="toast.id"
-      :message="toast.message"
-      :details="toast.details"
-      :type="toast.type"
-      :duration="toast.duration"
-      @remove="toastStore.removeToast"
-      class="pointer-events-auto"
-    />
-  </div>
+  <Teleport to="body">
+    <div class="fixed top-4 right-4 z-[9999] flex flex-col gap-2 pointer-events-none">
+      <Toast
+        v-for="toast in toastStore.toasts"
+        :key="toast.id"
+        :id="toast.id"
+        :message="toast.message"
+        :details="toast.details"
+        :type="toast.type"
+        :duration="toast.duration"
+        @remove="toastStore.removeToast"
+        class="pointer-events-auto"
+      />
+    </div>
+  </Teleport>
 </template>
